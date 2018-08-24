74395c319eea4314b4ea72f823b0ce3e91f0d683
Maren Woodruff <marenwoodruff@Marens-MacBook-Pro.local>
2018-08-24 12:11:39 -0400

    bootstrap working

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log("working")
+
+const codaLinks = [
+    {
+        name: "Marc",
+        ghRepoURL: "",
+        ghPagesURL: ""
+    },
+    {
+        name: "Marc",
+        ghRepoURL: "",
+        ghPagesURL: ""
+    },
+    {
+        name: "Marc",
+        ghRepoURL: "",
+        ghPagesURL: ""
+    },
+]
